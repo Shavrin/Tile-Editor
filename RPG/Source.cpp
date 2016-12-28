ed226445faa420374567857cc646ec1b3236cbcd
Shavrin <kacper.olek@gmail.com>
2016-12-28 01:18:30 +0100

    major update

--- RPG/Source.cpp
+++ RPG/Source.cpp
@@ -1,32 +1,67 @@
 #include <iostream>
-
 #include "SFML\Audio.hpp"
 #include "SFML\Graphics.hpp"
 #include "SFML\Window.hpp"
 
+#include "world.h"
 
 int main() {
 	sf::RenderWindow mainWindow(sf::VideoMode(540, 480), "RPG");
 
+	World world;
+	sf::View view0(sf::Vector2f(540.f / 2.f, 480.f / 2.f), sf::Vector2f(540.f, 480.f));
+
+	sf::View view1(sf::Vector2f(540.f/4.f,480.f/4.f), sf::Vector2f(540.f / 2.f, 480.f / 2.f));
+	sf::View view2(sf::Vector2f(3*540.f / 4.f,480.f / 4.f), sf::Vector2f(540.f / 2.f, 480.f / 2.f));
+	sf::View view3(sf::Vector2f(540.f / 4.f, 480.f*3 / 4.f), sf::Vector2f(540.f / 2.f, 480.f / 2.f));
+	sf::View view4(sf::Vector2f(540.f*3 / 4.f, 480.f*3 / 4.f), sf::Vector2f(540.f / 2.f, 480.f / 2.f));
 
 	while (mainWindow.isOpen()) {
 		sf::Event mainEvent;
-		sf::RectangleShape redRect(sf::Vector2f(50, 50));
-		redRect.setFillColor(sf::Color::Red);
-		redRect.setPosition(540 / 2, 480 / 2);
-		
+
 
 		while (mainWindow.pollEvent(mainEvent)) {
 			switch (mainEvent.type) {
 			case sf::Event::Closed:
 				mainWindow.close();
+				break;
+
+			case sf::Event::KeyPressed:
+				if (mainEvent.key.code == sf::Keyboard::L) {
+					//world.randomize();
+
+					world.loadFromFile();
+					world.showMapTiles();
+				}
+				if (mainEvent.key.code == sf::Keyboard::Escape) {
+					mainWindow.close();
+				}
+
+				if (mainEvent.key.code == sf::Keyboard::Num0) {
+					mainWindow.setView(view0);
+				}
+				if (mainEvent.key.code == sf::Keyboard::Num4) {
+					mainWindow.setView(view4);
+				}
+				
+				if (mainEvent.key.code == sf::Keyboard::Num3) {
+					mainWindow.setView(view3);
+
+				}
+				if (mainEvent.key.code == sf::Keyboard::Num2) {
+					mainWindow.setView(view2);
+
+				}
+				if (mainEvent.key.code == sf::Keyboard::Num1) {
+					mainWindow.setView(view1);
+
+				}
 			}
 		}
 		
 		mainWindow.clear(sf::Color::Magenta);
-		mainWindow.draw(redRect);
 		
-		
+		world.draw(mainWindow);
 		
 		mainWindow.display();
 
--- RPG/world.h
+++ RPG/world.h
@@ -1,28 +1,131 @@
 #pragma once
 #include "SFML\Graphics.hpp"
 #include <vector>
+#include <fstream>
 
+#include <iostream>
+#include <random>
 
-class world {
+class World {
 public:
 	enum _tileType
 	{
-		sand,ground
+		red,blue,green,yellow
 	};
 
-	sf::Vector2i mapTiles[100][100];
-
-
-
-
-	world(int X,int Y) {
+	_tileType mapTiles[18][18];
 	
+	std::fstream tilesFile;
+	
+	sf::RectangleShape tileBlue, tileGreen, tileYellow, tileRed;
+	World() {
+		tileBlue.setFillColor(sf::Color::Blue);
+		tileRed.setFillColor(sf::Color::Red);
+		
+		tileGreen.setFillColor(sf::Color::Green);
+		tileYellow.setFillColor(sf::Color::Yellow);
+		
+		tileBlue.setSize(sf::Vector2f(540 / 18, 480.f/ 18.f));
+
+		tileRed.setSize(sf::Vector2f(540 / 18, 480.f / 18.f));
+		tileYellow.setSize(sf::Vector2f(540 / 18, 480.f / 18.f));
+		tileGreen.setSize(sf::Vector2f(540 / 18, 480.f / 18.f));
+
+		//randomize();
+
+	}
+	void randomize() {
+		tilesFile.open("tiles.txt");
+		std::random_device generator;
+		std::uniform_int_distribution<int> distribution(0, 3);
+		int currTile;
+		for (int i = 0; i < 18; i++) {
+			for (int j = 0; j < 18; j++) {
+				currTile = distribution(generator);
+				tilesFile << currTile;
+				tilesFile << " ";
+			}
+			if(i != 17)
+			tilesFile << "\n";
+			
+		}
+		tilesFile.close();
+		showMapTiles();
+	}
+	void showMapTiles() {
+		for (int i = 0; i < 18;i++) {
+			for (int j = 0; j < 18; j++) {
+				std::cout << mapTiles[i][j];
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\n";
+
+	}
+	void isOpen() {
+		
+		if (tilesFile.is_open()) {
+			std::cout << "Otwarty!";
+		}
+		else std::cout << "Zamkniety!";
+	}
+	void loadFromFile() {
+		tilesFile.open("tiles.txt",std::ios::in);
+		if (tilesFile.is_open()) {
+			
+			int currNumber;
+			char currChar;
+			int lineNumber = 0;
+			int counter = 0;
+			while(!tilesFile.eof()){
+				if (counter >= 18) { counter = 0; lineNumber++; }
+				tilesFile >> currChar;
+				currNumber = currChar - '0';
+
+				mapTiles[counter][lineNumber] = (World::_tileType)currNumber;
+				counter++;
+
+			
+			
+			}
+			tilesFile.close();
+		}
+		else std::cout << "Couldn't open file\n";
+
 	}
 
-	void initializeWorld() {
+	void initialize() {
 		
 	}
 
+	
+	void draw(sf::RenderWindow &window) {
+
+		float width = 540 / 18;
+		float height = 480.f / 18.f;
+		for (int i = 0; i < 18; i++) {
+			for (int j = 0; j < 18; j++) {
+				switch (mapTiles[i][j]) {
+				case blue:
+					tileBlue.setPosition(sf::Vector2f(i * width,j * height));
+					window.draw(tileBlue);
+					break;
+				case red:
+					tileRed.setPosition(sf::Vector2f(i * width, j * height));
+					window.draw(tileRed);
+					break;
+				case yellow:
+					tileYellow.setPosition(sf::Vector2f(i * width, j * height));
+					window.draw(tileYellow);
+					break;
+				case green:
+					tileGreen.setPosition(sf::Vector2f(i * width, j * height));
+					window.draw(tileGreen);
+					break;
+				}
+			}
+		}
+	}
 
 	
 
